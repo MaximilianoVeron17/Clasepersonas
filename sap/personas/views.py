b8b35fa6e7fb93858c8495eda012991d2af086ed
usuario github <correo>
2021-06-29 01:42:52 -0300

    Initial commit

---
+++ sap/personas/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from personas.models import Persona
+from django.forms import modelform_factory
+# Create your views here.
+def detallePersona(request,id):
+    # persona=Persona.objects.get(pk=id)
+    persona=get_object_or_404(Persona,pk=id)
+    return render(request,'personas/detalle.html',{'persona': persona})
+
+PersonaForm= modelform_factory(Persona,exclude=[])
+def nuevaPersona(request):
+    if request.method=='POST':
+        formaPersona=PersonaForm(request.POST)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
+    else:
+        formaPersona= PersonaForm()
+
+    return render(request,'personas/nuevo.html',{'formaPersona':formaPersona})
---
+++ sap/webapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from personas.models import Persona
+# Create your views here.
+def bienvenido(request):
+    no_personas= Persona.objects.count()
+    personas= Persona.objects.order_by('id')
+
+    return render(request,'bienvenido.html',{'no_personas': no_personas,'personas':personas})
